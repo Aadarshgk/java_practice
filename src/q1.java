c0411ef521fa345a7ce1cc414068fbb54e5148a9
Aadarshgk <aadarshgk04@gmail.com>
2024-01-15 19:28:29 +0530

    Added practice program

---
+++ src/q1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class q1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int count=1,sum=0,num;
+        while(count<=5){
+            try {
+                System.out.print("Enter number #" + count);
+                num= in.nextInt();
+                sum+=num;
+                count++;
+            }catch(Exception e){
+                System.out.print("Invalid number");
+            }
+        }
+        System.out.println("The sum is:"+sum);
+
+    }
+}
