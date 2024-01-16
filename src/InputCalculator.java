3fb60fa66a49cfd60fda48f6eaaceccce6a480bd
Aadarshgk <aadarshgk04@gmail.com>
2024-01-16 16:33:28 +0530

    Added input calculator program

--- src/InputCalculator.java
+++ src/InputCalculator.java
@@ -1,9 +1,25 @@
 import java.util.*;
 public class InputCalculator {
-    Scanner in = new Scanner(System.in);
 
+    public static void inputThenPrintSumAndAverage(){
+        Scanner scanner = new Scanner(System.in);
+        int sum=0,avg=0,count=0,num;
+        double temp;
+        String x;
+        boolean flag= false;
+        while (!flag) {
+            x= scanner.nextLine();
+            try{
+                num= Integer.parseInt(x);
+                count++;
+                sum+=num;
+                temp= Math.round((double)sum/count);
+                avg= (int)temp;
+            }catch (NumberFormatException e){
+                System.out.println("SUM = "+sum+" AVG = "+avg);
+                flag=true;
+            }
 
-    public static void inputThenPrintSumAndAverage() {
-
+        }
     }
 }
