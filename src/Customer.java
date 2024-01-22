cb1aef00252e9d224c684b758c6eb58330004963
Aadarshgk <aadarshgk04@gmail.com>
2024-01-22 22:13:04 +0530

    Added customer program

---
+++ src/Customer.java
@@ -0,0 +1,24 @@
+public class Customer {
+    private String name;
+    private int creditLimit;
+    private String emailAddress;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCreditLimit() {
+        return creditLimit;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public Customer(String name, int creditLimit, String emailAddress) {
+        this.name = name;
+        this.creditLimit = creditLimit;
+        this.emailAddress = emailAddress;
+    }
+}
+
