7410206775da9536b30019176791b28e4f9d57b5
Aadarshgk <aadarshgk04@gmail.com>
2024-04-08 16:27:07 +0530

    Added box patter printing

--- src/Pattern2.java
+++ src/Pattern2.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Pattern2 {
+import java.util.*;
+public class Pattern2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        for (int i = 1; i <= a; i++) {
+            if (i % a < 2) {
+                for (int j = 1; j <= b; j++) {
+                    System.out.print("*");
+                }
+            } else {
+                for (int j = 1; j <= b; j++) {
+                    if (j == 1 || j == b) {
+                        System.out.print("*");
+                    }else{
+                        System.out.print(" ");
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
 }
