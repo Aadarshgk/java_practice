2eafde87d8f9996ccc20c6c6b441e0df3b16b191
Aadarshgk <aadarshgk04@gmail.com>
2024-04-08 21:34:12 +0530

    Added pattern program

---
+++ src/Pattern13.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Pattern13 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        for (int i = 1; i <= a; i++) {
+            for (int j = 1; j <= a - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
