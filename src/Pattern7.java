affd56041dfffb45cc23c3112b8cbb487eb0bd77
Aadarshgk <aadarshgk04@gmail.com>
2024-04-08 19:34:49 +0530

    Added Pattern program

---
+++ src/Pattern7.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Pattern7 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        for (int i = 0; i < a; i++) {
+            for (int j = 1; j <=a - i; j++) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
