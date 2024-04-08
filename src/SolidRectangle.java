7d4b73e2685616e95b2c5f873de455511760f0f7
Aadarshgk <aadarshgk04@gmail.com>
2024-04-08 16:14:37 +0530

    Added Solid rectangle pattern

---
+++ src/SolidRectangle.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class SolidRectangle {
+    public static void main(String[] args) {
+        Scanner in = new Scanner((System.in));
+        System.out.print("Enter the number of rows and columns");
+        int a= in.nextInt();
+        int b= in.nextInt();
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
