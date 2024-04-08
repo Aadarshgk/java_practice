5378d3db21ddb0aef82969a4242add58830febb8
Aadarshgk <aadarshgk04@gmail.com>
2024-04-08 19:42:14 +0530

    Added pattern program

--- src/Pattern9.java
+++ src/Pattern9.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Pattern9 {
+import java.util.Scanner;
+
+public class Pattern9 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j <= i; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("1 ");
+                }else{
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
