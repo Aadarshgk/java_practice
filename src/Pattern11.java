101a603ff5c04fff0fb18b1df535340426a6e3e3
Aadarshgk <aadarshgk04@gmail.com>
2024-04-08 21:29:47 +0530

    Added pattern program

--- src/Pattern11.java
+++ src/Pattern11.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Pattern11 {
+import java.util.Scanner;
+
+public class Pattern11 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        for (int i = 0; i < a; i++) {
+            for (int j = 1; j <= a - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < a; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
