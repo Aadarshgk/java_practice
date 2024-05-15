81806e81c638a624f95051e7fb327a5cc70fea3a
Aadarshgk <aadarshgk04@gmail.com>
2024-05-15 23:24:04 +0530

    Added quick sort

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,10 +1,10 @@
 public class QuickSort {
-    public static void swap(int ar[], int i, int j) {
+    public static void swap(int[] ar, int i, int j) {
         int temp = ar[i];
         ar[i] = ar[j];
         ar[j] = temp;
     }
-    public static int partition(int ar[], int low, int high) {
+    public static int partition(int[] ar, int low, int high) {
         int pivot = ar[high];
         int i = low - 1;
         for (int j = low; j < high; j++) {
@@ -18,7 +18,7 @@ public class QuickSort {
         return i;
     }
 
-    public static void quickSort(int ar[], int low, int high) {
+    public static void quickSort(int[] ar, int low, int high) {
         if (low < high) {
             int index = partition(ar, low, high);
             quickSort(ar, low, index - 1);
@@ -29,9 +29,9 @@ public class QuickSort {
     public static void main(String[] args) {
         int[] ar = {6, 2, 7, 3, 8, 9};
         quickSort(ar, 0, ar.length - 1);
-        for (int i = 0; i < ar.length; i++) {
-            System.out.print(ar[i] + " ");
+        for (int j : ar) {
+            System.out.print(j + " ");
         }
-        System.out.println();
+        System.out.println( );
     }
 }
