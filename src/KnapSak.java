aa65b43841b759919e889aa7a61642f2c1e9ccf4
Aadarshgk <aadarshgk04@gmail.com>
2024-08-08 23:45:00 +0530

    Added knapsak

--- src/KnapSak.java
+++ src/KnapSak.java
@@ -19,7 +19,7 @@ public class KnapSak {
         }
     }
 
-    public static void item(List<Integer> res,int maxProfit,int[] profit) {
+    public static void item(List<Integer> res,int maxProfit,int[] profit,int[] weight) {
         int row = ar.length;
         int col = ar[0].length-1;
         int i = row - 1;
@@ -27,6 +27,7 @@ public class KnapSak {
             if(ar[i][col]!=ar[i-1][col]){
                 res.add(i);
                 maxProfit -= profit[i - 1];
+                col-= weight[i-1];
             }
             i--;
         }
@@ -34,14 +35,14 @@ public class KnapSak {
 
 
     public static void main(String[] args) {
-        int[] profit= {60,100,120};
-        int[] weight = {10, 20, 30};
-        int w = 50;
+        int[] profit= {10,20,50,60};
+        int[] weight = {2,3,4,5};
+        int w = 8;
         ArrayList<Integer> res = new ArrayList<>();
         knapsak(profit, weight, w);
         int maxProfit = ar[profit.length][w];
         System.out.println("The maximum profit is: " + maxProfit);
-        item(res, maxProfit, profit);
+        item(res, maxProfit, profit,weight);
         System.out.println("The items to included are: ");
         for (int a : res) {
             System.out.print(a + " ");
