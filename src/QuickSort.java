ae9e072dad067a6394e3722da9b38766e91b95a7
Aadarshgk <aadarshgk04@gmail.com>
2024-08-08 13:45:16 +0530

    Added Quick Sort algorithm

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -26,9 +26,31 @@ public class QuickSort {
         }
     }
 
+
+    public static void quickSort1(int[] ar, int low, int high) {
+        if(low<=high){
+            int pivot=low;
+            int i = low + 1, j = high;
+            while (i <= j) {
+                while (i <= high && ar[i]<= ar[pivot]) {
+                    i++;
+                }
+                while (j > low && ar[j] > ar[pivot]) {
+                    j--;
+                }
+                if (i <= j) {
+                    swap(ar, i, j);
+                }
+            }
+            swap(ar,j,pivot);
+            quickSort1(ar, low, j - 1);
+            quickSort1(ar, j + 1, high);
+        }
+    }
+
     public static void main(String[] args) {
         int[] ar = {6, 2, 7, 3, 8, 9};
-        quickSort(ar, 0, ar.length - 1);
+        quickSort1(ar, 0, ar.length - 1);
         for (int j : ar) {
             System.out.print(j + " ");
         }
